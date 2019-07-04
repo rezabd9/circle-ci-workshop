d4c962781628ba785c2c71085163fc26ee4656d0
rezabd9 <rezabd9@gmail.com>
2019-07-04 11:42:05 +0200

    add item price

--- src/main/java/net/praqma/codeacademy/gildedrose/Item.java
+++ src/main/java/net/praqma/codeacademy/gildedrose/Item.java
@@ -7,15 +7,17 @@ public class Item {
     public int sellIn;
 
     public int quality;
+    public int price;
 
-    public Item(String name, int sellIn, int quality) {
+    public Item(String name, int sellIn, int quality,int price) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        this.price=price;
     }
 
    @Override
    public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+        return this.name + ", " + this.sellIn + ", " + this.quality+ ", " + this.price;
     }
 }
